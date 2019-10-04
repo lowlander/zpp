363e56be97072f9e975763f5b842421f74af566c
Erwin Rol <erwin@erwinrol.com>
2019-10-04 20:07:14 +0200

    Add tests for the borrowed_sem class
    Signed-off-by: Erwin Rol <erwin@erwinrol.com>

--- tests/sem/src/main.cpp
+++ tests/sem/src/main.cpp
@@ -13,6 +13,10 @@
 
 namespace {
 
+K_SEM_DEFINE(bm_sem, 0, 10);
+
+zpp::borrowed_sem simple_borrowed_sem(&bm_sem);
+
 zpp::sem simple_sem(0, 10);
 
 void test_sem_take()
@@ -73,6 +77,64 @@ void test_sem_try_take_for_fails()
 	}
 }
 
+void test_sem_take_borrowed()
+{
+	simple_borrowed_sem.reset();
+
+	for (int i = 0; i < 5; i++) {
+		simple_borrowed_sem++;
+
+		auto signal_count = simple_borrowed_sem.count();
+		zassert_true(signal_count == (i + 1),
+			     "signal count missmatch Expected %d, got %d\n",
+			     (i + 1), signal_count);
+	}
+}
+
+void test_sem_try_take_borrowed()
+{
+	for (int i = 4; i >= 0; i--) {
+		auto ret_value = simple_borrowed_sem.try_take();
+		zassert_true(ret_value == true,
+			     "unable to do k_sem_take which returned %d\n",
+			     ret_value);
+
+		auto signal_count = simple_borrowed_sem.count();
+		zassert_true(signal_count == i,
+			     "signal count missmatch Expected %d, got %d\n",
+			     i, signal_count);
+	}
+}
+
+void test_sem_try_take_fails_borrowed()
+{
+	simple_borrowed_sem.reset();
+
+	for (int i = 4; i >= 0; i--) {
+		auto ret_value = simple_borrowed_sem.try_take();
+		zassert_true(ret_value == false,
+			     "k_sem_take returned when not possible");
+
+		auto signal_count = simple_borrowed_sem.count();
+		zassert_true(signal_count == 0U,
+			     "signal count missmatch Expected 0, got %d\n",
+			     signal_count);
+	}
+}
+
+void test_sem_try_take_for_fails_borrowed()
+{
+	using namespace std::chrono;
+
+	simple_borrowed_sem.reset();
+
+	for (int i = 4; i >= 0; i--) {
+		auto ret_value = simple_borrowed_sem.try_take_for(100ms);
+		zassert_true(ret_value == false,
+			     "k_sem_take succeeded when its not possible");
+	}
+}
+
 } // namespace
 
 void test_main(void)
@@ -81,7 +143,11 @@ void test_main(void)
 			 ztest_unit_test(test_sem_take),
 			 ztest_unit_test(test_sem_try_take),
 			 ztest_unit_test(test_sem_try_take_fails),
-			 ztest_unit_test(test_sem_try_take_for_fails)
+			 ztest_unit_test(test_sem_try_take_for_fails),
+			 ztest_unit_test(test_sem_take_borrowed),
+			 ztest_unit_test(test_sem_try_take_borrowed),
+			 ztest_unit_test(test_sem_try_take_fails_borrowed),
+			 ztest_unit_test(test_sem_try_take_for_fails_borrowed)
 			);
 	ztest_run_test_suite(test_zpp_sem);
 }
