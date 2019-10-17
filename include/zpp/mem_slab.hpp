71b9757862b741263cb9a34e23267c6d204f9ead
Erwin Rol <erwin@erwinrol.com>
2019-10-17 19:25:24 +0200

    Rewrite mem_slab to be an Allocator
    Signed-off-by: Erwin Rol <erwin@erwinrol.com>

--- include/zpp/mem_slab.hpp
+++ include/zpp/mem_slab.hpp
@@ -10,189 +10,202 @@
 #include <kernel.h>
 #include <sys/__assert.h>
 
-#include <chrono>
-#include <limits>
-#include <new>
-#include <memory>
-#include <algorithm>
-
 namespace zpp {
 
 ///
-/// @brief compile time helper to calculate alignment
+/// @brief Allocator that uses k_mem_slab for memory
 ///
-template<typename ItemType>
-constexpr auto mem_slab_calc_item_alignment() noexcept
-{
-	auto start = alignof(void*);
-	auto end = std::max(start, alignof(ItemType));
-
-	while (start < end) {
-		start *= 2;
-	}
-
-	return start;
-}
-
+/// @param Type the type this Allocator supports
 ///
-/// @brief compile time helper to calculate size
-///
-template<typename ItemType>
-constexpr auto mem_slab_calc_item_size() noexcept
-{
-	auto start = mem_slab_calc_item_alignment<ItemType>();
-	auto end = std::max(start, sizeof(ItemType));
-
-	while (start < end) {
-		start += start;
-	}
-
-	return start;
-}
-
-///
-/// @brief memeory slab
-///
-/// @param ItemType the type of the objects this slab produces
-/// @param ItemCount the number of items this slab holds
-/// @param ItemAlignment the alignment of the items
-/// @param ItemSize the size of the items
-///
-template<typename ItemType,
-	 size_t ItemCount,
-	 size_t ItemAlignment = mem_slab_calc_item_alignment<ItemType>(),
-	 size_t ItemSize = mem_slab_calc_item_size<ItemType>()>
-class mem_slab
-{
+template <class Type>
+class borrowed_mem_slab {
 public:
-	struct mem_slab_deleter {
-		void operator()(ItemType* p) const noexcept {
-			if (p != nullptr && m_slab != nullptr) {
-				p->~ItemType();
-				void* vp = (void*)p;
-				k_mem_slab_free(m_slab->native_handle(), &vp);
-			}
-		}
-
-		mem_slab* m_slab{ nullptr };
-	};
-
-	using unique_ptr = std::unique_ptr<ItemType, mem_slab_deleter>;
+	using value_type = Type;
+	using pointer = Type*;
+	using const_pointer = const pointer;
+	using size_type = u32_t;
 public:
+	borrowed_mem_slab() = delete;
+
 	///
-	/// @brief mem_slab constructor
+	/// @brief create borrowed_mem_slab using an excisting k_mem_slab
 	///
-	mem_slab() noexcept
+	/// @param mem_slab to use for memory allocation
+	///
+	constexpr borrowed_mem_slab(k_mem_slab* mem_slab) noexcept
+		: m_mem_slab(mem_slab)
 	{
-		k_mem_slab_init(&m_slab, m_slab_buffer, ItemSize, ItemCount);
+		__ASSERT_NO_MSG(m_mem_slab != nullptr);
+		__ASSERT_NO_MSG(m_mem_slab->block_size >= sizeof(value_type));
+	}
+
+	template <class U>
+	constexpr borrowed_mem_slab(const borrowed_mem_slab<U>& other) noexcept
+		: m_mem_slab(other.m_mem_slab)
+	{
+		__ASSERT_NO_MSG(m_mem_slab != nullptr);
+		__ASSERT_NO_MSG(max_size() > 0);
 	}
 
 	///
-	/// @brief get native zephyr k_mem_slab handle
+	/// @brief allocate memory for n times sizeof(Type)
 	///
-	/// @return k_mem_slab pointer
+	/// @param n the number Types to alloc
 	///
-	constexpr auto native_handle() noexcept
+	/// @return pointer to memory for n times Type
+	///
+	[[nodiscard]] pointer allocate(size_type n) noexcept
 	{
-		return &m_slab;
-	}
+		__ASSERT_NO_MSG(m_mem_slab != nullptr);
+		__ASSERT_NO_MSG(n <= max_size());
 
-	///
-	/// @brief alloc an ItemType waiting for ever if no block available
-	///
-	/// @param args the arguments to pass to the ItemType constructor
-	///
-	/// @return ItemType smart pointer or nulllptr on error
-	///
-	template<typename ...Args>
-	[[nodiscard]] auto
-	alloc(Args&& ...args) noexcept
-	{
-		return alloc_impl(K_FOREVER, std::forward<Args>(args)...);
-	}
+		void* vp{nullptr};
 
-	///
-	/// @brief alloc an ItemType without waiting
-	///
-	/// @param args the arguments to pass to the ItemType constructor
-	///
-	/// @return ItemType smart pointer or nulllptr on error
-	///
-	template<typename ...Args>
-	[[nodiscard]] auto
-	try_alloc(Args&& ...args) noexcept
-	{
-		return alloc_impl(K_NO_WAIT, std::forward<Args>(args)... );
-	}
-
-	///
-	/// @brief alloc an ItemType waiting with a timeout
-	///
-	/// @param timeout the timeout time
-	/// @param args the arguments to pass to the ItemType constructor
-	///
-	/// @return ItemType smart pointer or nulllptr on error
-	///
-	template<class Rep, class Period, typename ...Args>
-	[[nodiscard]] auto
-	try_alloc_for(const std::chrono::duration<Rep, Period>& timeout,
-							Args&& ...args) noexcept
-	{
-		using namespace std::chrono;
-
-		return alloc_impl(duration_cast<milliseconds>(timeout).count(),
-						std::forward<Args>(args)...);
-	}
-
-	///
-	/// @brief get the free block count
-	///
-	/// @return number of free blocks
-	///
-	auto free_block_count() noexcept
-	{
-		return k_mem_slab_num_free_get(&m_slab);
-	}
-
-	///
-	/// @brief get the used block count
-	///
-	/// @return number of used blocks
-	///
-	auto used_block_count() noexcept
-	{
-		return k_mem_slab_num_used_get(&m_slab);
-	}
-
-	///
-	/// @brief get the total number of blocks
-	///
-	/// @return total number of blocks
-	///
-	constexpr auto total_block_count() noexcept
-	{
-		return ItemCount;
-	}
-private:
-	template<typename ...Args>
-	auto alloc_impl(s32_t timeout, Args&& ...args) noexcept
-	{
-		void* res{nullptr};
-		auto rc = k_mem_slab_alloc(&m_slab, &res, timeout);
+		auto rc = k_mem_slab_alloc(m_mem_slab, &vp, K_FOREVER);
 
 		if (rc == 0) {
-			auto p = new(res) ItemType{std::forward<Args>(args)...};
-
-			return unique_ptr(p, mem_slab_deleter{this});
+			return static_cast<pointer>(vp);
 		} else {
-			return unique_ptr(nullptr, mem_slab_deleter{this});
+			return nullptr;
 		}
 	}
+
+	///
+	/// @brief deallocate memory
+	///
+	/// @param p the pointer to free
+	/// @param n the number of Type (not used)
+	///
+	void deallocate(pointer p, size_type) noexcept
+	{
+		__ASSERT_NO_MSG(m_mem_slab != nullptr);
+		__ASSERT_NO_MSG(p != nullptr);
+
+		void* vp = p;
+		k_mem_slab_free(m_mem_slab, &vp);
+	}
+
+	///
+	/// @brief get number Type's that can be allocated
+	///
+	/// @return the max number of Types that can be allocated
+	///
+	constexpr size_type max_size() const noexcept
+	{
+		__ASSERT_NO_MSG(m_mem_slab != nullptr);
+
+		return m_mem_slab->block_size / sizeof(value_type);
+	}
+
+	///
+	/// @brief get maxium number of blocks that can be allocated
+	///
+	/// @return the maxium number of blocks that can be allocated
+	///
+	constexpr auto total_block_count() noexcept {
+		__ASSERT_NO_MSG(m_mem_slab != nullptr);
+
+		return m_mem_slab->num_blocks;
+	}
+
+	///
+	/// @brief get current number of used blocks
+	///
+	/// @return the current number of used blocks
+	///
+	constexpr auto used_block_count() noexcept {
+		__ASSERT_NO_MSG(m_mem_slab != nullptr);
+
+		return k_mem_slab_num_used_get(m_mem_slab);
+	}
+
+	///
+	/// @brief get current number of free blocks
+	///
+	/// @return the current number of free blocks
+	///
+	constexpr auto free_block_count() noexcept
+	{
+		__ASSERT_NO_MSG(m_mem_slab != nullptr);
+
+		return k_mem_slab_num_free_get(m_mem_slab);
+	}
 private:
-	alignas(ItemAlignment) char m_slab_buffer[ItemSize * ItemCount];
-	struct k_mem_slab m_slab{};
+	k_mem_slab*	m_mem_slab { nullptr };
 };
 
+
+///
+/// @brief compare if two borrowed_mem_pool can be mixed
+///
+/// @return always false
+///
+template <class T, class U>
+constexpr bool
+operator==(const borrowed_mem_slab<T>&, const borrowed_mem_slab<U>&) noexcept
+{
+	return false;
+}
+
+///
+/// @brief compare if two borrowed_mem_pool can be mixed
+///
+/// @return always true
+///
+template <class T, class U>
+constexpr bool
+operator!=(const borrowed_mem_slab<T>&, const borrowed_mem_slab<U>&) noexcept
+{
+	return true;
+}
+
+template <typename Type, size_t ContCount>
+constexpr size_t mem_slab_block_size() noexcept
+{
+	static_assert(ContCount > 0);
+
+	constexpr auto block_size = sizeof(Type) * ContCount;
+
+	// must be a multiple of 4
+	return ((block_size + 3) / 4) * 4;
+}
+
+template <typename Type, size_t ContCount, size_t MemSize>
+constexpr size_t mem_slab_block_count() noexcept
+{
+	constexpr auto block_size = mem_slab_block_size<Type, ContCount>();
+
+	static_assert(MemSize >= block_size);
+
+	return MemSize / block_size;
+}
+
 } // namespace zpp
+
+
+///
+/// @brief define a borrowed_mem_slab
+///
+/// @param name the name of the object
+/// @param Type the type of the objects in the pool
+/// @param ContCount the continuous number of Type's that are supported
+/// @param MemSize the size of the pool in bytes
+///
+#define ZPP_MEM_SLAB_DEFINE(name, Type, ContCount, MemSize) \
+	\
+	namespace { \
+	constexpr auto block_size_##name = ::zpp::mem_slab_block_size< \
+							Type, \
+							ContCount>(); \
+	constexpr auto block_count_##name = ::zpp::mem_slab_block_count< \
+							Type, \
+							ContCount, \
+							MemSize>(); \
+	K_MEM_SLAB_DEFINE(native_##name, \
+		block_size_##name, \
+		block_count_##name, \
+		alignof(Type)); \
+	} \
+	::zpp::borrowed_mem_slab<Type> name(&native_##name)
 
 #endif // ZPP__INCLUDE__ZPP__MEM_SLAB_HPP
--- tests/mem_slab/src/main.cpp
+++ tests/mem_slab/src/main.cpp
@@ -13,84 +13,26 @@
 
 namespace {
 
-int g_constructor_count {0};
-int g_destructor_count {0};
-
-class item {
-public:
-	item() noexcept
-	{
-		g_constructor_count++;
-	}
-
-	item(int a, int b) noexcept
-		: m_a(a), m_b(b)
-	{
-		g_constructor_count++;
-	}
-
-	item(const item& other) noexcept
-		: m_a(other.m_a), m_b(other.m_b)
-	{
-		g_constructor_count++;
-	}
-
-	item(item&& other) noexcept
-		: m_a(std::move(other.m_a)), m_b(std::move(other.m_b))
-	{
-		g_constructor_count++;
-	}
-
-	~item() noexcept {
-		g_destructor_count++;
-	}
-public:
-	int	m_a{ 1234 };
-	int	m_b{ 4321 };
-};
-
-zpp::mem_slab<item, 64> g_mem_slab;
+ZPP_MEM_SLAB_DEFINE(g_mem_slab, int, 1, 1024);
 
 void test_mem_slab()
 {
-	zassert_equal(g_mem_slab.total_block_count(), 64, "");
-	zassert_equal(g_mem_slab.free_block_count(), 64, "");
+	const auto total = g_mem_slab.total_block_count();
+
+	zassert_equal(g_mem_slab.free_block_count(), total, "");
 	zassert_equal(g_mem_slab.used_block_count(), 0, "");
 
-	zassert_equal(g_constructor_count, 0, "");
-	zassert_equal(g_destructor_count, 0, "");
-
-	auto p = g_mem_slab.try_alloc();
-
-	zassert_equal(g_mem_slab.free_block_count(), 63, "");
-	zassert_equal(g_mem_slab.used_block_count(), 1, "");
-
-	zassert_equal(g_constructor_count, 1, "");
-	zassert_equal(g_destructor_count, 0, "");
+	auto p = g_mem_slab.allocate(1);
 
 	zassert_not_null(p, "");
 
-	zassert_equal(p->m_a, 1234, "");
-	zassert_equal(p->m_b, 4321, "");
-
-	p->m_a = 5678;
-	p->m_b = 8765;
-
-	zassert_equal((*p).m_a, 5678, "");
-	zassert_equal((*p).m_b, 8765, "");
-
-	p = g_mem_slab.try_alloc(123456789, 987654321);
-
-	zassert_equal(g_mem_slab.free_block_count(), 63, "");
+	zassert_equal(g_mem_slab.free_block_count(), total - 1, "");
 	zassert_equal(g_mem_slab.used_block_count(), 1, "");
 
-	zassert_equal(g_constructor_count, 2, "");
-	zassert_equal(g_destructor_count, 1, "");
+	g_mem_slab.deallocate(p, 1);
 
-	zassert_not_null(p, "");
-
-	zassert_equal(p->m_a, 123456789, "");
-	zassert_equal(p->m_b, 987654321, "");
+	zassert_equal(g_mem_slab.free_block_count(), total, "");
+	zassert_equal(g_mem_slab.used_block_count(), 0, "");
 }
 
 } // namespace
