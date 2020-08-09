2a81438d0ac2a45d2d18bcc611126adaab6b6986
Erwin Rol <erwin@erwinrol.com>
2020-08-09 22:15:29 +0200

    Update coding style of sample code
    Signed-off-by: Erwin Rol <erwin@erwinrol.com>

--- samples/hello_world/src/main.cpp
+++ samples/hello_world/src/main.cpp
@@ -24,81 +24,81 @@ zpp::thread_data<1024> my_thread_tcb;
 
 int main(int argc, char *argv[])
 {
-	//
-	// use zpp and std::chrono namespaces in the function
-	//
-	using namespace zpp;
-	using namespace std::chrono;
+  //
+  // use zpp and std::chrono namespaces in the function
+  //
+  using namespace zpp;
+  using namespace std::chrono;
 
-	mutex print_lock;
+  mutex print_lock;
 
-	//
-	// Create thread attributes used for thread creation
-	//
-	const thread_attr my_thread_attr(
-				thread_prio::preempt(0),
-				thread_inherit_perms::no,
-				thread_suspend::no
-			);
+  //
+  // Create thread attributes used for thread creation
+  //
+  const thread_attr my_thread_attr(
+        thread_prio::preempt(0),
+        thread_inherit_perms::no,
+        thread_suspend::no
+      );
 
-	//
-	// Create the first thread, using a lambda passing a const char* as
-	// arument that is used as "template" for the print out.
-	//
-	// The lock used for making sure the prints don't get messed up
-	// is captured by reference
-	//
-	const char* string_arg = "Hello World from thread tid={}\n";
+  //
+  // Create the first thread, using a lambda passing a const char* as
+  // argument that is used as "template" for the print out.
+  //
+  // The lock used for making sure the prints don't get messed up
+  // is captured by reference
+  //
+  const char* string_arg = "Hello World from thread tid={}\n";
 
-	auto my_thread = thread(
-		my_thread_tcb, my_thread_attr,
-		[&print_lock](const char* t)
-		{
-			while (true) {
-				//
-				// Use a lock_guard in a scope to automatically
-				// do the unlocking when leaving the scope
-				//
-				{
-					lock_guard lg(print_lock);
+  auto my_thread = thread(
+    my_thread_tcb, my_thread_attr,
+    [&print_lock](const char* t)
+    {
+      while (true) {
+        //
+        // Use a lock_guard in a scope to automatically
+        // do the unlocking when leaving the scope
+        //
+        {
+          lock_guard lg(print_lock);
 
-					//
-					// print the thread ID of this thread
-					//
-					print(t, this_thread::get_id());
-				}
+          //
+          // print the thread ID of this thread
+          //
+          print(t, this_thread::get_id());
+        }
 
-				//
-				// let this thread sleep for 500 ms without
-				// holding the lock
-				//
-				this_thread::sleep_for(500ms);
-			}
-		}, string_arg);
+        //
+        // let this thread sleep for 500 ms without
+        // holding the lock
+        //
+        this_thread::sleep_for(500ms);
+      }
+    }, string_arg);
 
-	//
-	// Loop forever, becuase ending main would not only end the
-	// program, my_thread would also go out of scope aborting the thread.
-	//
-	while (true) {
-		// Use a lock_guard in a scope to automatically
-		// do the unlocking when leaving the scope
-		//
-		{
-			lock_guard lg(print_lock);
+  //
+  // Loop forever, because ending main would not only end the
+  // program, my_thread would also go out of scope aborting the thread.
+  //
+  while (true) {
+    // Use a lock_guard in a scope to automatically
+    // do the unlocking when leaving the scope
+    //
+    {
+      lock_guard lg(print_lock);
 
-			//
-			// print the thread ID of the main thread
-			//
-			print("Hello World from main tid={}\n",
-							this_thread::get_id());
-		}
+      //
+      // print the thread ID of the main thread
+      //
+      print("Hello World from main tid={}\n",
+              this_thread::get_id());
+    }
 
-		//
-		// let main thread sleep for 1 s without holding the lock
-		//
-		this_thread::sleep_for(1s);
-	}
+    //
+    // let main thread sleep for 1 s without holding the lock
+    //
+    this_thread::sleep_for(1s);
+  }
 
-	return 0;
+  return 0;
 }
