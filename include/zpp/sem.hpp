a2a470fc33516966c08e0b3360d949ecc364a45f
Erwin Rol <erwin@erwinrol.com>
2021-08-29 20:52:27 +0200

    Cleanup sem code
    Signed-off-by: Erwin Rol <erwin@erwinrol.com>

--- include/zpp/sem.hpp
+++ include/zpp/sem.hpp
@@ -20,17 +20,21 @@ namespace zpp {
 ///
 /// @brief Counting semaphore base class
 ///
-template<typename Sem>
+template<typename T_Sem>
 class sem_base
 {
 public:
+  using native_type = struct k_sem;
+  using native_pointer = native_type*;
+  using native_const_pointer = native_type const *;
+
   ///
   /// @brief Type used as counter
   ///
   using counter_type = uint32_t;
 
   ///
-  /// @brief Maxium value of the counter
+  /// @brief Maximum value of the counter
   ///
   constexpr static counter_type max_count =
         std::numeric_limits<counter_type>::max();
@@ -77,9 +81,9 @@ public:
   ///
   /// @return true when semaphore was taken
   ///
-  template < class Rep, class Period>
+  template<class T_Rep, class T_Period>
   [[nodiscard]] bool
-  try_take_for(const std::chrono::duration<Rep, Period>&
+  try_take_for(const std::chrono::duration<T_Rep, T_Period>&
             timeout_duration) noexcept
   {
     if (k_sem_take(native_handle(), to_timeout(timeout_duration)) == 0) {
@@ -160,13 +164,23 @@ public:
   }
 
   ///
-   /// @brief get the native zephyr sem handle.
+  /// @brief get the native zephyr sem handle.
   ///
   /// @return A pointer to the zephyr k_sem.
-   ///
-  auto native_handle() noexcept
+  ///
+  auto native_handle() noexcept -> native_pointer
   {
-    return static_cast<Sem*>(this)->native_handle();
+    return static_cast<T_Sem*>(this)->native_handle();
+  }
+
+  ///
+  /// @brief get the native zephyr sem handle.
+  ///
+  /// @return A pointer to the zephyr k_sem.
+  ///
+  auto native_handle() const noexcept -> native_const_pointer
+  {
+    return static_cast<const T_Sem*>(this)->native_handle();
   }
 public:
   sem_base(const sem_base&) = delete;
@@ -221,12 +235,22 @@ public:
   ///
   /// @return A pointer to the zephyr k_mutex.
   ///
-  constexpr auto native_handle() noexcept
+  constexpr auto native_handle() noexcept -> native_pointer
+  {
+    return &m_sem;
+  }
+
+  ///
+  /// @brief get the native zephyr mutex handle.
+  ///
+  /// @return A pointer to the zephyr k_mutex.
+  ///
+  constexpr auto native_handle() const noexcept -> native_const_pointer
   {
     return &m_sem;
   }
 private:
-  struct k_sem m_sem;
+  native_type m_sem;
 public:
   sem(const sem&) = delete;
   sem(sem&&) = delete;
@@ -237,18 +261,62 @@ public:
 ///
 /// @brief A counting semaphore class borrowing the native sem.
 ///
-class borrowed_sem : public sem_base<borrowed_sem> {
+class sem_ref : public sem_base<sem_ref> {
 public:
   ///
-  /// @brief Contruct a sem using a native k_sem*
+  /// @brief Construct a sem using a native k_sem*
   ///
   /// @param s The k_sem to use. @a s must already be
   ///          initialized and will not be freed.
   ///
-  explicit constexpr borrowed_sem(struct k_sem* s) noexcept
+  explicit constexpr sem_ref(native_pointer s) noexcept
     : m_sem_ptr(s)
   {
     __ASSERT_NO_MSG(m_sem_ptr != nullptr);
+  }
+
+  ///
+  /// @brief Construct a sem using a native k_sem*
+  ///
+  /// @param s The k_sem to use. @a s must already be
+  ///          initialized and will not be freed.
+  ///
+  template<class T_Sem>
+  explicit constexpr sem_ref(T_Sem& s) noexcept
+    : m_sem_ptr(s.native_handle())
+  {
+    __ASSERT_NO_MSG(m_sem_ptr != nullptr);
+  }
+
+  ///
+  /// @brief Assign a sem using a native k_sem*
+  ///
+  /// @param s The k_sem to use. @a s must already be
+  ///          initialized and will not be freed.
+  ///
+  /// @return reference to this object
+  ///
+  constexpr sem_ref& operator=(native_pointer s) noexcept
+  {
+    m_sem_ptr = s;
+    __ASSERT_NO_MSG(m_sem_ptr != nullptr);
+    return *this;
+  }
+
+  ///
+  /// @brief Assign a sem using another sem object
+  ///
+  /// @param s The sem to use. @a s must already be
+  ///          initialized and will not be freed.
+  ///
+  /// @return reference to this object
+  ///
+  template<class T_Sem>
+  constexpr sem_ref& operator=(T_Sem& s) noexcept
+  {
+    m_sem_ptr = s.native_handle();
+    __ASSERT_NO_MSG(m_sem_ptr != nullptr);
+    return *this;
   }
 
   ///
@@ -256,19 +324,28 @@ public:
   ///
   /// @return A pointer to the zephyr k_sem.
   ///
-  constexpr auto native_handle() noexcept
+  constexpr auto native_handle() noexcept -> native_pointer
+  {
+    __ASSERT_NO_MSG(m_sem_ptr != nullptr);
+
+    return m_sem_ptr;
+  }
+
+  ///
+  /// @brief get the native zephyr sem handle.
+  ///
+  /// @return A pointer to the zephyr k_sem.
+  ///
+  constexpr auto native_handle() const noexcept -> native_const_pointer
   {
     __ASSERT_NO_MSG(m_sem_ptr != nullptr);
 
     return m_sem_ptr;
   }
 private:
-  struct k_sem* m_sem_ptr{ nullptr };
+  native_pointer m_sem_ptr{ nullptr };
 public:
-  borrowed_sem(const borrowed_sem&) = delete;
-  borrowed_sem(borrowed_sem&&) = delete;
-  borrowed_sem& operator=(const borrowed_sem&) = delete;
-  borrowed_sem& operator=(borrowed_sem&&) = delete;
+  sem_ref() = delete;
 };
 
 } // namespace zpp
