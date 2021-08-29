005611cfeabc12ef22ae11236458dd19bacbd19b
Erwin Rol <erwin@erwinrol.com>
2021-08-29 20:48:24 +0200

    Update compile test
    Signed-off-by: Erwin Rol <erwin@erwinrol.com>

--- tests/compile/src/main.cpp
+++ tests/compile/src/main.cpp
@@ -14,7 +14,37 @@
 
 #include <zpp.hpp>
 
+//
+// check compile time power_of_two function
+//
+static_assert(zpp::power_of_two(0) == 1);
+static_assert(zpp::power_of_two(1) == 2);
+static_assert(zpp::power_of_two(2) == 4);
+static_assert(zpp::power_of_two(3) == 8);
+static_assert(zpp::power_of_two(4) == 16);
+static_assert(zpp::power_of_two(10) == 1024);
+static_assert(zpp::power_of_two(20) == 1048576);
+
+//
+// check compile time is_power_of_two function
+//
+static_assert(zpp::is_power_of_two(1) == true);
+static_assert(zpp::is_power_of_two(2) == true);
+static_assert(zpp::is_power_of_two(4) == true);
+static_assert(zpp::is_power_of_two(8) == true);
+static_assert(zpp::is_power_of_two(16) == true);
+static_assert(zpp::is_power_of_two(3) == false);
+static_assert(zpp::is_power_of_two(100) == false);
+
+//
+// check compile time is_multiple_of function
+//
+static_assert(zpp::is_multiple_of(0, 0) == false);
+static_assert(zpp::is_multiple_of(4, 4) == true);
+static_assert(zpp::is_multiple_of(6, 3) == true);
+static_assert(zpp::is_multiple_of(10, 3) == false);
+
 void test_main(void)
 {
-	/* This is only a compile time test */
+  /* This is only a compile time test */
 }
