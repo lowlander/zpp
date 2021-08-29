3f78f3f773cfdd32f12022f357ec4d7c70da5822
Erwin Rol <erwin@erwinrol.com>
2021-08-29 20:47:47 +0200

    Update mem_slab test
    Signed-off-by: Erwin Rol <erwin@erwinrol.com>

--- tests/mem_slab/src/main.cpp
+++ tests/mem_slab/src/main.cpp
@@ -9,39 +9,38 @@
 #include <ztest.h>
 
 #include <zpp/mem_slab.hpp>
-#include <zpp/fmt.hpp>
 
 namespace {
 
-ZPP_MEM_SLAB_DEFINE(g_mem_slab, int, 1, 1024);
+zpp::mem_slab<8, 64, 8> g_mem_slab;
 
 void test_mem_slab()
 {
-	const auto total = g_mem_slab.total_block_count();
+  const auto total = g_mem_slab.total_block_count();
 
-	zassert_equal(g_mem_slab.free_block_count(), total, "");
-	zassert_equal(g_mem_slab.used_block_count(), 0, "");
+  zassert_equal(g_mem_slab.free_block_count(), total, "");
+  zassert_equal(g_mem_slab.used_block_count(), 0, "");
 
-	auto p = g_mem_slab.allocate(1);
+  auto p = g_mem_slab.allocate();
 
-	zassert_not_null(p, "");
+  zassert_not_null(p, "");
 
-	zassert_equal(g_mem_slab.free_block_count(), total - 1, "");
-	zassert_equal(g_mem_slab.used_block_count(), 1, "");
+  zassert_equal(g_mem_slab.free_block_count(), total - 1, "");
+  zassert_equal(g_mem_slab.used_block_count(), 1, "");
 
-	g_mem_slab.deallocate(p, 1);
+  g_mem_slab.deallocate(p);
 
-	zassert_equal(g_mem_slab.free_block_count(), total, "");
-	zassert_equal(g_mem_slab.used_block_count(), 0, "");
+  zassert_equal(g_mem_slab.free_block_count(), total, "");
+  zassert_equal(g_mem_slab.used_block_count(), 0, "");
 }
 
 } // namespace
 
 void test_main()
 {
-	ztest_test_suite(zpp_mem_slab_tests,
-			 ztest_unit_test(test_mem_slab)
-		);
+  ztest_test_suite(zpp_mem_slab_tests,
+      ztest_unit_test(test_mem_slab)
+    );
 
-	ztest_run_test_suite(zpp_mem_slab_tests);
+  ztest_run_test_suite(zpp_mem_slab_tests);
 }
