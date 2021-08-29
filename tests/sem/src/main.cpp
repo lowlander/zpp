5427a730a2c0f4493c54ce7b3b5797fa5c154c89
Erwin Rol <erwin@erwinrol.com>
2021-08-29 20:46:22 +0200

    Update sem test
    Signed-off-by: Erwin Rol <erwin@erwinrol.com>

--- tests/sem/src/main.cpp
+++ tests/sem/src/main.cpp
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2019 Erwin Rol <erwin@erwinrol.com>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+* Copyright (c) 2021 Erwin Rol <erwin@erwinrol.com>
+*
+* SPDX-License-Identifier: Apache-2.0
+*/
 
 #include <zephyr.h>
 #include <kernel.h>
@@ -10,144 +10,188 @@
 
 #include <zpp/sem.hpp>
 #include <zpp/thread.hpp>
+#include <zpp/utils.hpp>
 
 namespace {
 
-K_SEM_DEFINE(bm_sem, 0, 10);
+K_SEM_DEFINE(g_sem, 0, 10);
 
-zpp::borrowed_sem simple_borrowed_sem(&bm_sem);
+zpp::sem_ref simple_ref_sem(&g_sem);
 
 zpp::sem simple_sem(0, 10);
 
+void test_sem_cmp()
+{
+  bool res;
+
+  res = simple_sem == simple_ref_sem;
+  zassert_false(res, "unable to compare sem == sem_ref\n");
+
+  res = simple_sem != simple_ref_sem;
+  zassert_true(res, "unable to compare sem != sem_ref\n");
+
+  res = simple_ref_sem == simple_sem;
+  zassert_false(res, "unable to compare sem_ref == sem\n");
+
+  res = simple_ref_sem != simple_sem;
+  zassert_true(res, "unable to compare sem_ref != sem\n");
+
+  res = simple_ref_sem == &g_sem;
+  zassert_true(res, "unable to compare sem_ref == k_sem*\n");
+
+  res = simple_ref_sem != &g_sem;
+  zassert_false(res, "unable to compare sem_ref != k_sem*\n");
+
+  res = simple_sem == &g_sem;
+  zassert_false(res, "unable to compare sem == k_sem*\n");
+
+  res = simple_sem != &g_sem;
+  zassert_true(res, "unable to compare sem != k_sem*\n");
+
+  res =  &g_sem == simple_ref_sem;
+  zassert_true(res, "unable to compare k_sem* == sem_ref\n");
+
+  res = &g_sem != simple_ref_sem;
+  zassert_false(res, "unable to compare k_sem* != sem_ref\n");
+
+  res = &g_sem == simple_sem;
+  zassert_false(res, "unable to compare k_sem* == sem\n");
+
+  res = &g_sem != simple_sem;
+  zassert_true(res, "unable to compare k_sem* != sem\n");
+
+}
+
 void test_sem_take()
 {
-	simple_sem.reset();
+  simple_sem.reset();
 
-	for (int i = 0; i < 5; i++) {
-		simple_sem++;
+  for (int i = 0; i < 5; i++) {
+    simple_sem++;
 
-		auto signal_count = simple_sem.count();
-		zassert_true(signal_count == (i + 1),
-			     "signal count missmatch Expected %d, got %d\n",
-			     (i + 1), signal_count);
-	}
+    auto signal_count = simple_sem.count();
+    zassert_true(signal_count == (i + 1),
+                 "signal count missmatch Expected %d, got %d\n",
+                 (i + 1), signal_count);
+  }
 }
 
 void test_sem_try_take()
 {
-	for (int i = 4; i >= 0; i--) {
-		auto ret_value = simple_sem.try_take();
-		zassert_true(ret_value == true,
-			     "unable to do k_sem_take which returned %d\n",
-			     ret_value);
+  for (int i = 4; i >= 0; i--) {
+    auto ret_value = simple_sem.try_take();
+    zassert_true(ret_value == true,
+                 "unable to do k_sem_take which returned %d\n",
+                 ret_value);
 
-		auto signal_count = simple_sem.count();
-		zassert_true(signal_count == i,
-			     "signal count missmatch Expected %d, got %d\n",
-			     i, signal_count);
-	}
+    auto signal_count = simple_sem.count();
+    zassert_true(signal_count == i,
+                 "signal count missmatch Expected %d, got %d\n",
+                 i, signal_count);
+  }
 }
 
 void test_sem_try_take_fails()
 {
-	simple_sem.reset();
+  simple_sem.reset();
 
-	for (int i = 4; i >= 0; i--) {
-		auto ret_value = simple_sem.try_take();
-		zassert_true(ret_value == false,
-			     "k_sem_take returned when not possible");
+  for (int i = 4; i >= 0; i--) {
+    auto ret_value = simple_sem.try_take();
+    zassert_true(ret_value == false,
+                 "k_sem_take returned when not possible");
 
-		auto signal_count = simple_sem.count();
-		zassert_true(signal_count == 0U,
-			     "signal count missmatch Expected 0, got %d\n",
-			     signal_count);
-	}
+    auto signal_count = simple_sem.count();
+    zassert_true(signal_count == 0U,
+                 "signal count missmatch Expected 0, got %d\n",
+                 signal_count);
+  }
 }
 
 void test_sem_try_take_for_fails()
 {
-	using namespace std::chrono;
+  using namespace std::chrono;
 
-	simple_sem.reset();
+  simple_sem.reset();
 
-	for (int i = 4; i >= 0; i--) {
-		auto ret_value = simple_sem.try_take_for(100ms);
-		zassert_true(ret_value == false,
-			     "k_sem_take succeeded when its not possible");
-	}
+  for (int i = 4; i >= 0; i--) {
+    auto ret_value = simple_sem.try_take_for(100ms);
+    zassert_true(ret_value == false,
+                 "k_sem_take succeeded when its not possible");
+  }
 }
 
-void test_sem_take_borrowed()
+void test_sem_take_ref()
 {
-	simple_borrowed_sem.reset();
+  simple_ref_sem.reset();
 
-	for (int i = 0; i < 5; i++) {
-		simple_borrowed_sem++;
+  for (int i = 0; i < 5; i++) {
+    simple_ref_sem++;
 
-		auto signal_count = simple_borrowed_sem.count();
-		zassert_true(signal_count == (i + 1),
-			     "signal count missmatch Expected %d, got %d\n",
-			     (i + 1), signal_count);
-	}
+    auto signal_count = simple_ref_sem.count();
+    zassert_true(signal_count == (i + 1),
+                 "signal count missmatch Expected %d, got %d\n",
+                 (i + 1), signal_count);
+  }
 }
 
-void test_sem_try_take_borrowed()
+void test_sem_try_take_ref()
 {
-	for (int i = 4; i >= 0; i--) {
-		auto ret_value = simple_borrowed_sem.try_take();
-		zassert_true(ret_value == true,
-			     "unable to do k_sem_take which returned %d\n",
-			     ret_value);
+  for (int i = 4; i >= 0; i--) {
+    auto ret_value = simple_ref_sem.try_take();
+    zassert_true(ret_value == true,
+                 "unable to do k_sem_take which returned %d\n",
+                 ret_value);
 
-		auto signal_count = simple_borrowed_sem.count();
-		zassert_true(signal_count == i,
-			     "signal count missmatch Expected %d, got %d\n",
-			     i, signal_count);
-	}
+    auto signal_count = simple_ref_sem.count();
+    zassert_true(signal_count == i,
+                 "signal count missmatch Expected %d, got %d\n",
+                 i, signal_count);
+  }
 }
 
-void test_sem_try_take_fails_borrowed()
+void test_sem_try_take_fails_ref()
 {
-	simple_borrowed_sem.reset();
+  simple_ref_sem.reset();
 
-	for (int i = 4; i >= 0; i--) {
-		auto ret_value = simple_borrowed_sem.try_take();
-		zassert_true(ret_value == false,
-			     "k_sem_take returned when not possible");
+  for (int i = 4; i >= 0; i--) {
+    auto ret_value = simple_ref_sem.try_take();
+    zassert_true(ret_value == false,
+                 "k_sem_take returned when not possible");
 
-		auto signal_count = simple_borrowed_sem.count();
-		zassert_true(signal_count == 0U,
-			     "signal count missmatch Expected 0, got %d\n",
-			     signal_count);
-	}
+    auto signal_count = simple_ref_sem.count();
+    zassert_true(signal_count == 0U,
+                 "signal count missmatch Expected 0, got %d\n",
+                 signal_count);
+  }
 }
 
-void test_sem_try_take_for_fails_borrowed()
+void test_sem_try_take_for_fails_ref()
 {
-	using namespace std::chrono;
+  using namespace std::chrono;
 
-	simple_borrowed_sem.reset();
+  simple_ref_sem.reset();
 
-	for (int i = 4; i >= 0; i--) {
-		auto ret_value = simple_borrowed_sem.try_take_for(100ms);
-		zassert_true(ret_value == false,
-			     "k_sem_take succeeded when its not possible");
-	}
+  for (int i = 4; i >= 0; i--) {
+    auto ret_value = simple_ref_sem.try_take_for(100ms);
+    zassert_true(ret_value == false,
+                 "k_sem_take succeeded when its not possible");
+  }
 }
 
 } // namespace
 
 void test_main(void)
 {
-	ztest_test_suite(test_zpp_sem,
-			 ztest_unit_test(test_sem_take),
-			 ztest_unit_test(test_sem_try_take),
-			 ztest_unit_test(test_sem_try_take_fails),
-			 ztest_unit_test(test_sem_try_take_for_fails),
-			 ztest_unit_test(test_sem_take_borrowed),
-			 ztest_unit_test(test_sem_try_take_borrowed),
-			 ztest_unit_test(test_sem_try_take_fails_borrowed),
-			 ztest_unit_test(test_sem_try_take_for_fails_borrowed)
-			);
-	ztest_run_test_suite(test_zpp_sem);
+  ztest_test_suite(test_zpp_sem,
+      ztest_unit_test(test_sem_cmp),
+      ztest_unit_test(test_sem_take),
+      ztest_unit_test(test_sem_try_take),
+      ztest_unit_test(test_sem_try_take_fails),
+      ztest_unit_test(test_sem_try_take_for_fails),
+      ztest_unit_test(test_sem_take_ref),
+      ztest_unit_test(test_sem_try_take_ref),
+      ztest_unit_test(test_sem_try_take_fails_ref),
+      ztest_unit_test(test_sem_try_take_for_fails_ref)
+      );
+  ztest_run_test_suite(test_zpp_sem);
 }
