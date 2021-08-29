e0a49cd879627d8df4f6f40a7a684b6558965aa1
Erwin Rol <erwin@erwinrol.com>
2021-08-29 20:55:23 +0200

    Update needed C++ version to 20
    Signed-off-by: Erwin Rol <erwin@erwinrol.com>

--- include/zpp.hpp
+++ include/zpp.hpp
@@ -10,23 +10,29 @@
 #ifndef __cplusplus
 #error "ZPP is a C++ only library"
 #else
-#if __cplusplus < 201703L
-#error "ZPP needs C++17 or newer"
+#if __cplusplus < 201709L
+#error "ZPP needs C++20 or newer"
 #endif
 #endif
 
 #include <zpp/atomic_bitset.hpp>
 #include <zpp/atomic_var.hpp>
 #include <zpp/clock.hpp>
+#include <zpp/condition_variable.hpp>
 #include <zpp/fmt.hpp>
 #include <zpp/fifo.hpp>
-#include <zpp/mem_pool.hpp>
+#include <zpp/heap.hpp>
 #include <zpp/mem_slab.hpp>
+#include <zpp/futex.hpp>
 #include <zpp/mutex.hpp>
+#include <zpp/sys_mutex.hpp>
 #include <zpp/poll.hpp>
 #include <zpp/sched.hpp>
 #include <zpp/sem.hpp>
 #include <zpp/thread.hpp>
 #include <zpp/timer.hpp>
+#include <zpp/lock_guard.hpp>
+#include <zpp/utils.hpp>
+#include <zpp/unique_lock.hpp>
 
 #endif // ZPP_INCLUDE_ZPP_HPP
