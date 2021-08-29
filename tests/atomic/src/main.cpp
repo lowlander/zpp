1dcffe5a20517216479469bf4cd7c6069895dd4b
Erwin Rol <erwin@erwinrol.com>
2021-08-29 20:49:08 +0200

    Update atomic test
    Signed-off-by: Erwin Rol <erwin@erwinrol.com>

--- tests/atomic/src/main.cpp
+++ tests/atomic/src/main.cpp
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2019 Erwin Rol <erwin@erwinrol.com>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+* Copyright (c) 2019 Erwin Rol <erwin@erwinrol.com>
+*
+* SPDX-License-Identifier: Apache-2.0
+*/
 
 #include <zephyr.h>
 #include <kernel.h>
@@ -17,20 +17,20 @@ zpp::atomic_bitset<320> g_bitset;
 
 void test_atomic_bitset()
 {
-	g_bitset.store(0, true);
-	zassert_true(g_bitset.load(0) == true, "load(0) failed");
+  g_bitset.store(0, true);
+  zassert_true(g_bitset.load(0) == true, "load(0) failed");
 
-	g_bitset.store(319, true);
-	zassert_true(g_bitset.load(319) == true, "load(319) failed");
+  g_bitset.store(319, true);
+  zassert_true(g_bitset.load(319) == true, "load(319) failed");
 }
 
 } // namespace
 
 void test_main()
 {
-	ztest_test_suite(zpp_atomic_tests,
-			 ztest_unit_test(test_atomic_bitset)
-		);
+  ztest_test_suite(zpp_atomic_tests,
+      ztest_unit_test(test_atomic_bitset)
+    );
 
-	ztest_run_test_suite(zpp_atomic_tests);
+  ztest_run_test_suite(zpp_atomic_tests);
 }
