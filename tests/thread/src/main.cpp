ada6921f1d55991701ce0a40f496585ae700622b
Erwin Rol <erwin@erwinrol.com>
2021-08-29 20:45:59 +0200

    Update thread test
    Signed-off-by: Erwin Rol <erwin@erwinrol.com>

--- tests/thread/src/main.cpp
+++ tests/thread/src/main.cpp
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2019 Erwin Rol <erwin@erwinrol.com>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+* Copyright (c) 2019 Erwin Rol <erwin@erwinrol.com>
+*
+* SPDX-License-Identifier: Apache-2.0
+*/
 
 #include <ztest.h>
 
@@ -20,96 +20,102 @@ zpp::thread_data<1024> tcb;
 
 static void test_thread_creation(void)
 {
-	using namespace zpp;
-	using namespace std::chrono;
+  using namespace zpp;
+  using namespace std::chrono;
 
-	const thread_attr attr(
-				thread_prio::preempt(0),
-				thread_inherit_perms::no,
-				thread_essential::no,
-				thread_suspend::no
-			);
+  const thread_attr attr(
+        thread_prio::preempt(0),
+        thread_inherit_perms::no,
+        thread_essential::no,
+        thread_suspend::no
+      );
 
-	sem done;
+  sem done;
 
-	auto t = thread(
-		tcb, attr,
-		[&done]() {
-			print("Hello from thread tid={}\n",
-						this_thread::get_id());
+  auto t = thread(
+    tcb, attr,
+    [&done]() {
+      print("Hello from thread tid={}\n",
+            this_thread::get_id());
 
-			done++;
-		});
+      done++;
+    });
 
-	// wait until the thread does done++
-	done--;
+  // wait until the thread does done++
+  done--;
 
-	print("Hello from main tid={}\n", this_thread::get_id());
+  auto  rc = t.join();
+  zassert_true(rc, "join failed");
+
+  print("Hello from main tid={}\n", this_thread::get_id());
 }
 
 static void test_thread_creation_params(void)
 {
-	using namespace zpp;
-	using namespace std::chrono;
+  using namespace zpp;
+  using namespace std::chrono;
 
-	const thread_attr attr(
-				thread_prio::preempt(0),
-				thread_inherit_perms::no,
-				thread_essential::no,
-				thread_suspend::no
-			);
+  const thread_attr attr(
+        thread_prio::preempt(0),
+        thread_inherit_perms::no,
+        thread_essential::no,
+        thread_suspend::no
+      );
 
-	struct S {
-		S() {
-			print("S() {} {} {}",
-				(void*)this, a, b);
-		}
+  struct S {
+    S() {
+      print("S() {} {} {}",
+        (void*)this, a, b);
+    }
 
-		S(const S& other) : a(other.a), b(other.b) {
-			print("S(&{} {} {}) {} {} {}",
-				(void*)&other, other.a, other.b,
-				(void*)this, a, b);
-		}
-		~S() {
-			print("~S() {} {} {}",
-				(void*)this, a, b);
-		}
+    S(const S& other) : a(other.a), b(other.b) {
+      print("S(&{} {} {}) {} {} {}",
+        (void*)&other, other.a, other.b,
+        (void*)this, a, b);
+    }
+    ~S() {
+      print("~S() {} {} {}",
+        (void*)this, a, b);
+    }
 
-		int a{};
-		int b{};
-	};
+    int a{};
+    int b{};
+  };
 
-	sem done;
+  sem done;
 
-	S s;
-	int a = 12;
-	int b = 34;
+  S s;
+  int a = 12;
+  int b = 34;
 
-	auto t = thread(
-		tcb, attr,
-		[&done](S s, int a, int b) {
-			print("Hello from thread tid={} s.a={} s.b={} a={} b={}\n",
-						this_thread::get_id(),
-						s.a, s.b, a, b);
+  auto t = thread(
+    tcb, attr,
+    [&done](S s, int a, int b) {
+      print("Hello from thread tid={} s.a={} s.b={} a={} b={}\n",
+            this_thread::get_id(),
+            s.a, s.b, a, b);
 
-			s.a = a;
-			s.b = b;
+      s.a = a;
+      s.b = b;
 
-			done++;
-		}, s, a, b);
+      done++;
+    }, s, a, b);
 
-	// wait until the thread does done++
-	done--;
+  // wait until the thread does done++
+  done--;
 
-	print("Hello from main tid={}\n", this_thread::get_id());
+  auto rc = t.join();
+  zassert_true(rc, "join failed");
+
+  print("Hello from main tid={}\n", this_thread::get_id());
 }
 
 void test_main(void)
 {
-	ztest_test_suite(zpp_thread_tests,
-			 ztest_unit_test(test_thread_creation),
-			 ztest_unit_test(test_thread_creation_params)
-		);
+  ztest_test_suite(zpp_thread_tests,
+      ztest_unit_test(test_thread_creation),
+      ztest_unit_test(test_thread_creation_params)
+    );
 
-	ztest_run_test_suite(zpp_thread_tests);
+  ztest_run_test_suite(zpp_thread_tests);
 }
