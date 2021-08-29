a123327c544a644bd82647f4cf5f254a7346418e
Erwin Rol <erwin@erwinrol.com>
2021-08-29 20:48:51 +0200

    Update clock test
    Signed-off-by: Erwin Rol <erwin@erwinrol.com>

--- tests/clock/src/main.cpp
+++ tests/clock/src/main.cpp
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2019 Erwin Rol <erwin@erwinrol.com>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+* Copyright (c) 2019 Erwin Rol <erwin@erwinrol.com>
+*
+* SPDX-License-Identifier: Apache-2.0
+*/
 
 #include <zephyr.h>
 #include <kernel.h>
@@ -16,24 +16,24 @@ namespace {
 
 void test_clock_uptime()
 {
-	using namespace std::chrono;
+  using namespace std::chrono;
 
-	auto start = zpp::uptime_clock::now();
+  auto start = zpp::uptime_clock::now();
 
-	zpp::this_thread::sleep_for(2s);
+  zpp::this_thread::sleep_for(2s);
 
-	auto end = zpp::uptime_clock::now();
+  auto end = zpp::uptime_clock::now();
 
-	zassert_true(end > start, "end time not later than start time");
+  zassert_true(end > start, "end time not later than start time");
 }
 
 } // namespace
 
 void test_main()
 {
-	ztest_test_suite(zpp_clock_tests,
-			 ztest_unit_test(test_clock_uptime)
-		);
+  ztest_test_suite(zpp_clock_tests,
+      ztest_unit_test(test_clock_uptime)
+    );
 
-	ztest_run_test_suite(zpp_clock_tests);
+  ztest_run_test_suite(zpp_clock_tests);
 }
