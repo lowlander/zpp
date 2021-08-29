9d79b3b1bdb8058524fc81fd8926ca6810196ec8
Erwin Rol <erwin@erwinrol.com>
2021-08-29 20:45:13 +0200

    Update timer test
    Signed-off-by: Erwin Rol <erwin@erwinrol.com>

--- tests/timer/src/main.cpp
+++ tests/timer/src/main.cpp
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2019 Erwin Rol <erwin@erwinrol.com>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+* Copyright (c) 2019 Erwin Rol <erwin@erwinrol.com>
+*
+* SPDX-License-Identifier: Apache-2.0
+*/
 
 #include <zephyr.h>
 #include <kernel.h>
@@ -18,44 +18,44 @@ auto g_t = zpp::make_timer();
 
 void timer_callback(zpp::timer_base* t) noexcept
 {
-	zpp::print("Hello from timer tid={}\n", zpp::this_thread::get_id());
+  zpp::print("Hello from timer tid={}\n", zpp::this_thread::get_id());
 }
 
 void test_timer_creation_function()
 {
-	using namespace zpp;
-	using namespace std::chrono;
+  using namespace zpp;
+  using namespace std::chrono;
 
-	auto t = make_timer(timer_callback);
-	t.start(100ms, 1s);
+  auto t = make_timer(timer_callback);
+  t.start(100ms, 1s);
 
-	this_thread::sleep_for(5s);
+  this_thread::sleep_for(5s);
 }
 
 void test_timer_creation_lambda()
 {
-	using namespace zpp;
-	using namespace std::chrono;
+  using namespace zpp;
+  using namespace std::chrono;
 
-	auto t = make_timer(
-		[] (auto t) {
-			print("Hello from timer tid={}\n",
-					this_thread::get_id());
-		} );
+  auto t = make_timer(
+    [] (auto t) {
+      print("Hello from timer tid={}\n",
+          this_thread::get_id());
+    } );
 
-	t.start(100ms, 1s);
+  t.start(100ms, 1s);
 
-	this_thread::sleep_for(5s);
+  this_thread::sleep_for(5s);
 }
 
 } // namespace
 
 void test_main()
 {
-	ztest_test_suite(zpp_timer_tests,
-			 ztest_unit_test(test_timer_creation_function),
-			 ztest_unit_test(test_timer_creation_lambda)
-		);
+  ztest_test_suite(zpp_timer_tests,
+      ztest_unit_test(test_timer_creation_function),
+      ztest_unit_test(test_timer_creation_lambda)
+    );
 
-	ztest_run_test_suite(zpp_timer_tests);
+  ztest_run_test_suite(zpp_timer_tests);
 }
